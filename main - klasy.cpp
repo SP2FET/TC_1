56e87f6086ef77c951f1a12b9a2548d871f6c18e
SP2FET <sp2fet@gmail.com>
2017-03-11 00:10:51 +0100

    wstępnie ogarnięte
    wstępnie ogarnięta klasa, możliwe dodawanie węzłów oraz krawędzi

---
+++ main - klasy.cpp
@@ -0,0 +1,150 @@
+#include <iostream>
+
+using namespace std;
+
+struct node_t
+{
+	struct edge_t{
+	  edge_t *next_edge;
+	  node_t *node;
+	}*edge; //wskaznik na element listy krawedzi
+	
+	int number; //numer wezla
+		
+	node_t() {
+		edge_t *edge = new edge_t;
+		edge->next_edge = NULL;
+	}	
+	int get_number() { return number;}
+	
+	void create_edge(node_t *&parent, node_t *&child);
+	
+};
+
+void node_t::create_edge(node_t *&parent, node_t *&child)
+{
+	edge_t *new_edge = new edge_t;
+
+	new_edge->node = child;
+	if(parent->edge->next_edge == 0) //pierwszy element listy
+	{
+		parent->edge->next_edge = new_edge;
+	}
+	else
+	{
+
+		while(parent->edge->next_edge) //szukanie ostatniej krawedzi
+			parent->edge = parent->edge->next_edge;
+			
+		parent->edge->next_edge = new_edge;
+	}
+	
+	
+}
+
+struct graph_t
+{
+	node_t *actual_node;
+	node_t *first_node;
+	int nodes_amount;
+	
+	
+	int create_node(node_t *&parent_node);
+	graph_t();
+	int get_nodes_amount() { return nodes_amount; }
+
+};
+
+graph_t::graph_t()
+{
+	nodes_amount = 1;
+	node_t *new_init_node = new node_t;
+	new_init_node->number = 1;
+	new_init_node->edge->next_edge = 0;
+	new_init_node->edge->node = 0;
+	first_node = new_init_node;
+	actual_node = first_node;
+	
+}
+	
+
+
+int graph_t::create_node(node_t *&parent_node)
+{
+	cout<<"create init"<<endl;
+	node_t *node_to_add = new node_t;
+	
+	node_to_add->number = nodes_amount + 1;
+	
+	nodes_amount += 1;
+	//node_to_add->create_edge(parent_node, node_to_add);
+	cout<<"Node "<<node_to_add->number<<" created"<<endl;
+	return node_to_add->number;
+	
+}
+//
+//bool search_node(node *next_node, node *&found_node, short node_name) //jako pierwszy parametr podajemy wezel, od ktorego zaczynamy
+//{
+//    bool wanted_node_exists=false;
+//    node *bufor;
+//
+//        while(	1/*tu nie wiem co, trzeba jakos sprawdzic czy nie sprawdzilo sie juz wszystkich wezlow*/)
+//        {
+//            while(next_node->actual_connections->child.visited) // dopoki nastepny wezel z listy polaczen wezel zostal odwiedzony
+//            {
+//                if(next_node->actual_connections->next==NULL) //jesli dotarlismy do ostatniego polaczenia i bylo odwiedzone - wszystkie drogi juz odwiedzilismy
+//                {
+//                    next_node=previous_node; // powracamy do wezla, z ktorego przyszlismy
+//                    break;
+//                }
+//
+//                next_node->actual_connection=next_node->actual_connection->next; //sprawdzamy nastepne polaczenie
+//            }
+//
+//            bufor=next_node;
+//            next_node=next_node->actual_connection->child; // jesli odnazliono jakis nieodwiedzony wezel
+//            next_node->previous_node=bufor;
+//
+//            if(next_node.name!=node_name)
+//            {
+//                found_node=next_node;
+//                wanted_node_exists=true;
+//            }
+//
+//            next_node.visited=true;
+//        }
+//
+//        return wanted_node_exists;
+//}
+//
+//
+//bool add_connection(short child_name, short parent_name)
+//{
+//   node *parent;
+//   node *child;
+//
+//   if(search_node(parent,parent_name))
+//    {
+//
+//    }
+//     else cout<<"Nie znaleiono wezla-rodzica!"<<endl;
+//
+//}
+
+
+int main()
+{
+
+ //node *first_node=NULL;
+
+//	node_t *node = new node_t;
+	int num;
+	graph_t graph;
+	cout<<"init"<<endl;
+	num = graph.create_node(graph.actual_node);
+	
+	cout<<"node number = "<<graph.actual_node->number<<" New number: "<<num<<endl;
+	//delete graph;
+system("dir");
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -2,107 +2,123 @@
 
 using namespace std;
 
-class node_list
-{
+
+struct node_t 
+{				  
+	int visited;
+	struct edge_t *edges;
+	struct node_t *next_node;
 	
-	public:	
+	node_t();
 };
-class graph_c
+
+
+struct edge_t 
 {
+	  struct node_t *connected_node;
+	  struct edge_t *next_edge;
+};
+
+node_t::node_t()
+{
+	edge_t *edge = new edge_t;
+	edge->connected_node = 0;
+	edge->next_edge = 0;
+	edges = edge;
+}
+struct graph_t
+{
+	struct node_t *first_node;
+	int nodes_amount;
 	
+	graph_t();
+	void add_edge(node_t *&parent, node_t *&child);
+	void add_node(node_t *&parent);
+};
+
+graph_t::graph_t()
+{
+	node_t *init_node = new node_t;
+	init_node->visited = 0;
+
+	init_node->next_node = 0;	
 	
-	struct node_t
+	init_node->edges->connected_node = 0;
+	init_node->edges->next_edge = 0;
+	
+	nodes_amount = 1;
+	first_node = init_node;
+}			
+
+void graph_t::add_edge(node_t *&parent, node_t *&child)
+{
+	edge_t *new_edge_parent = new edge_t;
+	new_edge_parent->connected_node = child;
+	new_edge_parent->next_edge = 0;
+	node_t *temp_node;
+	
+	if(parent->edges->next_edge == 0)
 	{
-		//node_list_t *connected_nodes_list; //wskaznik na listê krawedzi
+		parent->edges->next_edge = new_edge_parent;
+	}else
+	{
+		temp_node = parent;
+		while(temp_node->edges->next_edge) //szukanie ostatniej krawedzi
+			  temp_node->edges = temp_node->edges->next_edge;
+		temp_node->edges->next_edge = new_edge_parent;
 		
-		node_t *last_node; //root wezla
-		bool visited;
-		//node_t *next_node; //root wezla
-		short name;
+	}
+	
+	edge_t *new_edge_child = new edge_t;
+	new_edge_child->connected_node = parent;
+	new_edge_child->next_edge = 0;
+	
+	if(child->edges->next_edge == 0)
+	{
+		child->edges->next_edge = new_edge_child;
 		
-	}node;
-	
-	public:
-	
-	
-	
+	}else
+	{
 		
-};
-
-
-
-
-
-
-
-struct connection
-{
- connection *next;
- node *child;
-};
-
-struct node
-{
- short name;
- bool visited=false;   // uzywane tylko przy przeszukiwaniu listy
- node *previous_node;  // uzywane tylko przy przeszukiwaniu listy
- connection *actual_connection;
-};
-
-bool search_node(node *next_node, node *&found_node, short node_name) //jako pierwszy parametr podajemy wezel, od ktorego zaczynamy
-{
-    bool wanted_node_exists=false;
-    node *bufor;
-
-        while(/*tu nie wiem co, trzeba jakos sprawdzic czy nie sprawdzilo sie juz wszystkich wezlow*/)
-        {
-            while(next_node->actual_connections->child.visited) // dopoki nastepny wezel z listy polaczen wezel zostal odwiedzony
-            {
-                if(next_node->actual_connections->next==NULL) //jesli dotarlismy do ostatniego polaczenia i bylo odwiedzone - wszystkie drogi juz odwiedzilismy
-                {
-                    next_node=previous_node; // powracamy do wezla, z ktorego przyszlismy
-                    break;
-                }
-
-                next_node->actual_connection=next_node->actual_connection->next; //sprawdzamy nastepne polaczenie
-            }
-
-            bufor=next_node;
-            next_node=next_node->actual_connection->child; // jesli odnazliono jakis nieodwiedzony wezel
-            next_node->previous_node=bufor;
-
-            if(next_node.name!=node_name)
-            {
-                found_node=next_node;
-                wanted_node_exists=true;
-            }
-
-            next_node.visited=true;
-        }
-
-        return wanted_node_exists;
+		temp_node = child;
+		while(child->edges->next_edge) //szukanie ostatniej krawedzi
+			  child->edges = child->edges->next_edge;
+		child->edges->next_edge = new_edge_child;
+	}
+	
 }
 
-
-bool add_connection(short child_name, short parent_name)
+void graph_t::add_node(node_t *&parent)
 {
-   node *parent;
-   node *child;
-
-   if(search_node(parent,parent_name))
-    {
-
-    }
-     else cout<<"Nie znaleiono wezla-rodzica!"<<endl;
-
+	node_t *new_node = new node_t;
+	node_t *temp_node;
+	new_node->visited = 0;
+	if(parent->next_node == 0) parent->next_node = new_node;
+	else 
+	{
+		temp_node = parent;
+		while(temp_node->next_node) //szukanie ostatniej krawedzi
+			  temp_node = temp_node->next_node;
+		temp_node->next_node = new_node;
+	}
+	new_node->next_node = 0;
+	add_edge(parent,new_node);
+	add_edge(new_node,parent);
+	nodes_amount +=1;
 }
-
-
 int main()
 {
-
- node *first_node=NULL;
-
-
+	graph_t *graph = new graph_t;
+	
+	graph->add_node(graph->first_node);
+	graph->add_node(graph->first_node->next_node);
+	graph->add_node(graph->first_node->next_node->next_node);
+	graph->add_edge(graph->first_node, graph->first_node->next_node->next_node);
+	for(int i = 1; i <= graph->nodes_amount; i++)
+	{
+		cout<<"node: "<<i<<" "<<endl;
+	}
+	delete graph;
+	cout<<"OK"<<endl;
     return 0;
 }
